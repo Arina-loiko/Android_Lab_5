8f973f6cd626a2b493763b4b61358e58aad81ae7
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-05-13 05:57:05 +0300

    add Row and Column with character loop

--- app/src/main/java/com/loiko/myapplication/MainActivity.kt
+++ app/src/main/java/com/loiko/myapplication/MainActivity.kt
@@ -5,9 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -21,17 +25,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Android_Lab_5_loikoTheme {
-                Text(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .padding(top = 48.dp, start = 16.dp, end = 16.dp)
-                        .width(100.dp),
-                    text = "Hello",
-                    fontWeight = FontWeight.ExtraBold,
-                    color = Color.Blue,
-                    fontSize = 20.sp,
-                    letterSpacing = 2.sp
-                )
+                val text = "Hello World".filter { it.isLetter() }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row {
+                        for (char in text) {
+                            Text(
+                                modifier = Modifier
+                                    .padding(8.dp)
+                                    .background(Color.Green),
+                                text = char.toString(),
+                                fontWeight = FontWeight.ExtraBold,
+                                color = Color.Blue,
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                }
             }
         }
     }
