179a60676b5553a1b51abd1f1e3a66dc1ac1c788
Arina-loiko <arina.loikoloiko@yandex.ru>
2026-05-13 05:57:05 +0300

    add Text with background and modifiers

--- app/src/main/java/com/loiko/myapplication/MainActivity.kt
+++ app/src/main/java/com/loiko/myapplication/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.loiko.myapplication.ui.theme.Android_Lab_5_loikoTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +21,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Android_Lab_5_loikoTheme {
+                Text(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .padding(top = 48.dp, start = 16.dp, end = 16.dp)
+                        .width(100.dp),
+                    text = "Hello",
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Blue,
+                    fontSize = 20.sp,
+                    letterSpacing = 2.sp
+                )
             }
         }
     }
